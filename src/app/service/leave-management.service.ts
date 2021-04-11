676eb02a2a2697bcc8edd832e4cf575ce17def70
priyanga <priamuru2099@gmail.com>
2021-04-11 14:29:32 -0700

    updated

--- src/app/service/leave-management.service.ts
+++ src/app/service/leave-management.service.ts
@@ -13,18 +13,18 @@ export class LeaveManagementService {
 
   constructor(private httpclient:HttpClient) { }
   Add(user:any):Observable<any>{
-  return this.httpclient.post<any>('http://localhost:3000/leave/',user,this.httpOptions);
+  return this.httpclient.post<any>('http://localhost:8080/leavemanagement/addLeave',user,this.httpOptions);
   }
   getAll(){
-    return this.httpclient.get<any>('http://localhost:3000/leave',this.httpOptions);
+    return this.httpclient.get<any>('http://localhost:8080/leavemanagement/leave',this.httpOptions);
   }
   getById(id:any):Observable<any>{
-    return this.httpclient.get<any>('http://localhost:3000/leave/'+id,this.httpOptions);
+    return this.httpclient.get<any>('http://localhost:8080/leavemanagement/leave/'+id,this.httpOptions);
   }
   delete(id:any):Observable<any>{
-    return this.httpclient.delete<any>('http://localhost:3000/leave/'+id,this.httpOptions);
+    return this.httpclient.delete<any>('http://localhost:8080/leavemanagement/deleteleave/'+id,this.httpOptions);
  }
  update(id:number,user:any):Observable<any>{
-  return this.httpclient.put<any>('http://localhost:3000/leave/'+id,user,this.httpOptions);
+  return this.httpclient.put<any>('http://localhost:8080/leavemanagement/updateleave/'+id,user,this.httpOptions);
 }
 }
